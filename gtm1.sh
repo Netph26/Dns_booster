dd529fa1a9fc712920b8796d78be14196dcdd277
Netph26 <154125651+Netph26@users.noreply.github.com>
2023-12-18 16:05:35 +0800

    Create gtm1.sh

---
+++ gtm1.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+## Copyright ©UDPTeam
+## Discord: https://discord.gg/civ3
+## Script to keep-alive your DNSTT server domain record query from target resolver/local dns server
+## Run this script excluded to your VPN tunnel (split vpn tunneling mode)
+## run command: ./globe-killfreenet3.sh l
+
+## Your DNSTT Nameserver & your Domain `A` Record
+NS='ns.james.palaboy.tech'
+A='james.palaboy.tech'
+## Repeat dig cmd loop time (seconds) (positive integer only)
+LOOP_DELAY=5
+
+## Add your DNS here
+declare -a HOSTS=('124.6.181.4')
+
+## Linux' dig command executable filepath
+## Select value: "CUSTOM|C" or "DEFAULT|D "
+DIG_EXEC="DEFAULT"
+## if set to CUSTOM, enter your custom dig executable path here
+CUSTOM_DIG="/data/data/com.termux/files/home/go/bin/fastdig"
+
+######################################
+######################################
+######################################
+######################################
+######################################
+VER=0.1
+
+case "${DIG_EXEC}" in
+  DEFAULT|D)
+    _DIG="$(command -v dig)"
+    ;;
+  CUSTOM|C)
+    _DIG="${CUSTOM_DIG}"
+    ;;
+esac
+
+if [ ! "$_DIG" ]; then
+  printf "%b" "Dig command failed to run. Please install dig(dnsutils) or check DIG_EXEC & CUSTOM_DIG variables inside $( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/$(basename "$0") file.\n" && exit 1
+fi
+
+endscript() {
+  unset NS A LOOP_DELAY HOSTS _DIG DIG_EXEC CUSTOM_DIG T R M
+  exit 1
+}
+
+trap endscript 2 15
+
+check(){
+  for ((i=0; i<"${#HOSTS[*]}"; i++)); do
+    for R in "${A}" "${NS}"; do
+      T="${HOSTS[$i]}"
+      timeout -k 3 3 ${_DIG} @${T} ${R} &> /dev/null && M=31 || M=32
+      echo -e "\e[${M}m${R} D:${T}\e[0m"
+      unset T R M
+    done
+  done
+}
+
+boost(){
+  local count="${1:-1}"
+  for ((i=0; i<count; i++)); do
+    check
+    sleep ${LOOP_DELAY}
+  done
+}
+
+echo "DNSTT Keep-Alive script <Discord @civ3>"
+echo -e "DNS List: [\e[34m${HOSTS[*]}\e[0m]"
+echo "CTRL + C to close script"
+
+[ "${LOOP_DELAY}" -eq 1 ] && let "LOOP_DELAY++"
+
+case "${1}" in
+  loop|l)
+    echo "Script loop: ${LOOP_DELAY} seconds"
+    while true; do
+      check
+      echo '.--. .-.. . .- ... .     .-- .- .. -'
+      sleep ${LOOP_DELAY}
+    done
+    ;;
+  -b|--boost)
+    if [ -n "${2}" ] && [ "${2}" -eq "${2}" ] 2>/dev/null; then
+      echo "Boosting DNS queries: ${2}"
+      boost "${2}"
+    else
+      echo "Invalid argument. Please provide a positive integer for the number of queries to boost."
+    fi
+    ;;
+  *)
+    check
+    ;;
+esac
+
+exit
