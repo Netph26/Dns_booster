80b0f747f5235097cf80bdf50cecc5f760339b4d
Netph26 <154125651+Netph26@users.noreply.github.com>
2023-12-18 18:07:37 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-clear
-
-# Your DNSTT Nameserver & your Domain `A` Record
+# Your DNSTT Nameserver & your Domain A Record
 NS='sdns.myudp.elcavlaw.com'
-
-# Repeat dig cmd loop time (seconds) (positive integer only)
-LOOP_DELAY=5
-
-# Number of parallel queries to boost
-NUM_PARALLEL=5
 
 # Add your DNS here
 declare -a HOSTS=('124.6.181.4')
+
+clear
 
 function endscript() {
   exit 1
@@ -20,43 +14,78 @@ function endscript() {
 
 trap endscript 2 15
 
-# ... (rest of the script remains unchanged)
+echo -e "\e[1;37mEnter DNS IPs separated by ' ': \e[0m"
+read -a DNS_IPS
+
+echo -e "\e[1;37mEnter Your NameServers separated by ' ': \e[0m"
+read -a NAME_SERVERS
+
+LOOP_DELAY=4
+echo -e "\e[1;37mCurrent loop delay is \e[1;33m${LOOP_DELAY}\e[1;37m seconds.\e[0m"
+echo -e "\e[1;37mWould you like to change the loop delay? \e[1;36m[y/n]:\e[0m "
+read -r change_delay
+
+if [[ "$change_delay" == "y" ]]; then
+  echo -e "\e[1;37mEnter custom loop delay in seconds \e[1;33m(5-15):\e[0m "
+  read -r custom_delay
+  if [[ "$custom_delay" =~ ^[5-9]$|^1[0-5]$ ]]; then
+    LOOP_DELAY=$custom_delay
+  else
+    echo -e "\e[1;31mInvalid input. Using default loop delay of ${LOOP_DELAY} seconds.\e[0m"
+  fi
+fi
+
+DIG_EXEC="DEFAULT"
+CUSTOM_DIG=/data/data/com.termux/files/home/go/bin/fastdig
+VER=0.3
+
+case "${DIG_EXEC}" in
+  DEFAULT|D)
+    _DIG="$(command -v dig)"
+    ;;
+  CUSTOM|C)
+    _DIG="${CUSTOM_DIG}"
+    ;;
+esac
+
+if [ ! $(command -v ${_DIG}) ]; then
+  printf "%b" "Dig command failed to run, please install dig(dnsutils) or check the DIG_EXEC & CUSTOM_DIG variable.\n" && exit 1
+fi
 
 # Initialize the counter
 count=1
 
-check_parallel() {
+check(){
   local border_color="\e[95m"  # Light magenta color
   local success_color="\e[92m"  # Light green color
   local fail_color="\e[91m"    # Light red color
+  local header_color="\e[96m"  # Light cyan color
   local reset_color="\e[0m"    # Reset to default terminal color
   local padding="  "            # Padding for aesthetic
 
   # Header
   echo -e "${border_color}┌────────────────────────────────────────────────┐${reset_color}"
-  echo -e "${border_color}│${reset_color}${padding}DNS Status Check Results (Parallel Boost)${padding}${reset_color}"
+  echo -e "${border_color}│${header_color}${padding}DNS Status Check Results${padding}${reset_color}"
   echo -e "${border_color}├────────────────────────────────────────────────┤${reset_color}"
   
   # Results
-  for host in "${HOSTS[@]}"; do
-    (
-      result=$(${_DIG} "@${host}" ${NS} +short)
+  for T in "${DNS_IPS[@]}"; do
+    for R in "${NAME_SERVERS[@]}"; do
+      result=$(${_DIG} @${T} ${R} +short)
       if [ -z "$result" ]; then
-        echo -e "${border_color}│${padding}${reset_color}DNS IP: ${host}${reset_color}"
-        echo -e "${border_color}│${padding}NameServer: ${NS}${reset_color}"
-        echo -e "${border_color}│${padding}Status: ${fail_color}Failed${reset_color}"
+        STATUS="${fail_color}Failed${reset_color}"
       else
-        echo -e "${border_color}│${padding}${reset_color}DNS IP: ${host}${reset_color}"
-        echo -e "${border_color}│${padding}NameServer: ${NS}${reset_color}"
-        echo -e "${border_color}│${padding}Status: ${success_color}Success${reset_color}"
+        STATUS="${success_color}Success${reset_color}"
       fi
-    ) &
+      echo -e "${border_color}│${padding}${reset_color}DNS IP: ${T}${reset_color}"
+      echo -e "${border_color}│${padding}NameServer: ${R}${reset_color}"
+      echo -e "${border_color}│${padding}Status: ${STATUS}${reset_color}"
+    done
   done
-  wait
 
   # Check count and Loop Delay
   echo -e "${border_color}├────────────────────────────────────────────────┤${reset_color}"
-  echo -e "${border_color}│${padding}${reset_color}Check count: ${count}${padding}${reset_color}"
+  echo -e "${border_color}│${padding}${header_color}Check count: ${count}${padding}${reset_color}"
   echo -e "${border_color}│${padding}Loop Delay: ${LOOP_DELAY} seconds${padding}${reset_color}"
   
   # Footer
@@ -75,7 +104,7 @@ clear
 
 # Main loop
 while true; do
-  check_parallel
+  check
   ((count++))  # Increment the counter
   sleep $LOOP_DELAY
 done
