88a4d1e1cdba8f1bc68b6551abc5f9d6cef4b334
Netph26 <154125651+Netph26@users.noreply.github.com>
2023-12-18 19:13:43 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,7 +9,10 @@ function endscript() {
 trap endscript 2 15
 
 # Your DNSTT Nameserver & your Domain A Record
+NS='sdns.myudp.elcavlaw.com'
+NS='sdns.myudp1.elcavlaw.com'
 NS='sdns.myudp2.elcavlaw.com'
+NS='sdns.myudph.elcavlaw.com'
 declare -a HOSTS=('124.6.181.4')
 
 LOOP_DELAY=4
